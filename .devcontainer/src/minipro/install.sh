3c5f8efface3cbb46428e03ce5db70ed1a125912
David <dhirsch1138@gmail.com>
2024-11-19 01:18:25 +0000

    Clean up devcontainer.json, move .c65 into feature?

--- .devcontainer/src/minipro/install.sh
+++ .devcontainer/src/minipro/install.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt install libusb-1.0-0-dev fakeroot debhelper dpkg-dev libb64-dev udev -y
 mkdir .tempbuild
 cd .tempbuild
 git clone https://gitlab.com/DavidGriffith/minipro.git
